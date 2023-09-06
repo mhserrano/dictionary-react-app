a7091684c7708b3883a4b29edcdbc433ef78610b
mhserrano <mariahcserrano@gmail.com>
2023-09-06 22:10:17 +0100

    Added the search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import SearchForm from "./SearchForm.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="text-center m-3">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <SearchForm />
+        </main>
+        <footer className="text-center m-3">Coded by Maria Serrano</footer>
+      </div>
     </div>
   );
 }
---
+++ src/SearchForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./SearchForm.css";
+
+export default function SearchForm() {
+  const [word, setWord] = useState("sunset");
+  function updateWord(event) {
+    setWord(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(word);
+  }
+
+  return (
+    <div className="search-form text-center">
+      <h2>What word do you want to look up?</h2>
+      <form className="d-block m-3" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type here..."
+          autoFocus={true}
+          onChange={updateWord}
+        ></input>
+        <input type="submit" value="Search"></input>
+      </form>
+    </div>
+  );
+}
