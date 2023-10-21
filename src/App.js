c7c56552eb4e84fd494c63dbfed50f06e7e4b5c9
mhserrano <mariahcserrano@gmail.com>
2023-10-21 23:39:35 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="text-center">
-          <h1>Dictionary</h1>
-        </header>
-        <main>
-          <Dictionary defaultKeyword="moon" />
-        </main>
+    <div>
+      <div className="App">
+        <div className="container">
+          <header className="text-center">
+            <h1>Dictionary</h1>
+          </header>
+          <main>
+            <Dictionary defaultKeyword="moon" />
+          </main>
+        </div>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <strong>
+          <a href="https://github.com/mhserrano">Maria Serrano</a>
+        </strong>{" "}
+        and is{" "}
+        <strong>
+          {" "}
+          <a href="https://github.com/mhserrano/dictionary-react-app">
+            open sourced
+          </a>
+        </strong>{" "}
+        on GitHub
+      </footer>
     </div>
   );
 }
